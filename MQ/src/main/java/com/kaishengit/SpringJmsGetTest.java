a4f75c9020f12560e5a52c931ad2772d84685996
jiang <1142007022@qq.com>
2018-05-08 15:31:06 +0800

    测试ActiveMQ

---
+++ MQ/src/main/java/com/kaishengit/SpringJmsGetTest.java
@@ -0,0 +1,17 @@
+package com.kaishengit;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+public class SpringJmsGetTest implements MessageListener {
+    public void onMessage(Message message) {
+        TextMessage textMessage = (TextMessage) message;
+        try {
+            System.out.println(textMessage.getText());
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+}
