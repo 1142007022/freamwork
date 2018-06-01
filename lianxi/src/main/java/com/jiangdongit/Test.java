c1a93993a01931ef545251c6eb8f580a5c4bd085
jiang <1142007022@qq.com>
2018-06-01 16:29:39 +0800

    修复bug

---
+++ lianxi/src/main/java/com/jiangdongit/Test.java
@@ -0,0 +1,10 @@
+package com.jiangdongit;
+
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println("3" + 4);
+        System.out.println(3 + "4");
+    }
+
+}
---
+++ love-1/src/main/java/com/jiangdongit/Love1Application.java
@@ -0,0 +1,12 @@
+package com.jiangdongit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Love1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Love1Application.class, args);
+	}
+}
