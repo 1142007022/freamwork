82679a7a9caf8887198358adaba0aa98b7c491c4
jiang <1142007022@qq.com>
2018-06-03 11:48:36 +0800

    搭建网盘项目  但是仍有404问题未解决

--- wangpan/src/main/java/com/jiangdongit/controller/HomeController.java
+++ wangpan/src/main/java/com/jiangdongit/controller/HomeController.java
@@ -2,13 +2,11 @@ package com.jiangdongit.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping()
+    @GetMapping("/")
     public String home(){
         return "index";
     }
