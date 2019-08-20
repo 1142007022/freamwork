d5dee13e9881a3937ced178384b4eab66eb1b1a3
jiang <1142007022@qq.com>
2019-08-20 10:36:55 +0800

    修改依赖

--- tms/tms-service-impl/src/main/java/com/kaishengit/Start.java
+++ tms/tms-service-impl/src/main/java/com/kaishengit/Start.java
@@ -11,7 +11,7 @@ public class Start {
     public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("spring*.xml");
         classPathXmlApplicationContext.start();
-        System.out.println("-------success--------");
+        System.out.println("-------main class start--------");
         System.in.read();
     }
 
